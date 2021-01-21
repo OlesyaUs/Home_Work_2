d40f7cfca0847e276bc65e45d7a5f73c3ff24a28
OlesyaUs <usol9999@gmail.com>
2021-01-21 21:54:58 +0600

    Сделала калькулятор

---
+++ df.py
@@ -0,0 +1,23 @@
+print('введите число number1=')
+number1 = int(input())
+print('ведите число number2=')
+number2 = int(input())
+operation = input("Введите операцию: ")
+
+if operation == "+":
+    print('Сумма ' + str(number1) + " + "+ str(number2) + " = " + str(number1+number2))
+elif operation == "-":
+    print('Разность number1-number2=', number1-number2)
+elif operation == "/":
+    print('Результат деления number1/number2=', number1/number2)
+elif operation == "*":
+    print('Результат умножения number1*number2=', number1*number2)
+elif operation == "//":
+    print('Результат целочисленного деления number1//number2=', number1 // number2)
+elif operation == "%":
+    print('Остаток от деления number1%number2=', number1 % number2)
+elif operation == "**":
+    print('Возведение в степень number1**number2=', number1 ** number2)
+
+
+
