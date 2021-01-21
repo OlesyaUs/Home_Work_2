56172c9e44b0d95861f34f1793a5a9d2dac0d257
OlesyaUs <usol9999@gmail.com>
2021-01-21 20:20:37 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+print("Hello World")
+print("")
+y: int= int (12390)
+x=int(input('введите число'))
+print(type(x))
+print(x)
+z="'Привет'"
+print(z)
+print(5/2)
+# Создаю переменную
+'''многострочный коментарий'''
+tovar =390
+tovar_=100
+print(tovar)
+x = int(input('20'))
+y = int(input('8'))
+x = x + y # 28
+y = x - y # 20
+x = x - y # 8
+
+
+print('введите число number1=')
+number1 = int(input())
+print('введите число number2=')
+number2 = int(input())
+print('Выберете операцию')
+print('1 - сложить number1+number2')
+print('2 - вычесть number1+number2')
+d = int(input())
+if d==1:
+    print('Сумма number1+number2', number1+number2)
+if d==2:
+    print('Разность number1+number2', number1+number2)
+
+
+
+
+
+
