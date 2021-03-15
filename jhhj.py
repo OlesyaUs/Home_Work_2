5b4e7fac55345e928e9d9b0e9e2855323e7c6995
OlesyaUs <usol9999@gmail.com>
2021-03-15 20:13:17 +0600

    boniry

--- jhhj.py
+++ jhhj.py
@@ -1,11 +1,18 @@
-# # i = 5
-# # while i < 15:
-# #     print(i)
-# #     i = i + 2
-# #
-# for  i in 'hello world':
-#     print(i * 2)
-#
-# i = 5
-# x = 10
-# while i < 15
+
+import tkinter as tk
+from PIL import ImageTk, Image
+
+
+root = tk.Tk()
+
+img = ImageTk.PhotoImage(Image.open('C:\Users\a\Desktop\поход2.jpg'))
+panel = tk.Label(root, image=img)
+panel.pack(side="bottom", fill="both", expand="yes")
+
+def callback(e):
+    img2 = ImageTk.PhotoImage(Image.open('img2.jpg'))
+    panel.configure(image=img2)
+    panel.image = img2
+
+root.bind("<Return>", callback)
+root.mainloop()
---
+++ month2/lessson6.py
@@ -0,0 +1,11 @@
+file = open("data.txt", "r")
+class Human:
+def __init__ (self, name, age, weight, size):
+    self.name = name
+    self.age = age
+    self.weight = weight
+    self.size = size
+
+
+
+for i in file.
